d1b137a467ef7def9ad933da71f3577e5ce58716
apparel3tech <appareldigitalocean@gmail.com>
2024-04-23 03:33:54 +0530

    init

--- app.js
+++ app.js
@@ -3,17 +3,13 @@ const app = express();
 const cors = require("cors");
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-// Middleware to set a cookie
 
-// app.use((req, res, next) => {
-//   res.cookie('username', 'JohnDoe', {
-//     maxAge: 900000, // Expires after 15 minutes
-//     // httpOnly: true
-//     secure:true // Makes the cookie accessible only via HTTP(S) requests
-//   });
-//   next();
-// });
 
+app.get('/' , (req , res)=>{
+
+   res.send('hello from simple server :)')
+
+})
 // Route to send a response with cookies
 app.post("/", (req, res) => {
   res.cookie("username", "JohnDoe", {
